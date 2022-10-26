39f6f7f775c4e656c1e0c0629a20f76368b5645a
Zan Hiroo <95255730+HIROS-DEV@users.noreply.github.com>
2022-10-26 19:55:10 +0900

    reset css in style.css

---
+++ script.js
@@ -0,0 +1,5 @@
+//TODO: 1. create a webpage with 16 x 16 grid of square divs
+// => create div using JavaScript (using createElement and add class name "container")
+// => create another div using JavaScript(using createElement and add class name "grid")
+// => attach the grid div inside the container div
+// => using Css and create Grid in HTML file(I think flexbox or CSS Grid is suitable for job)
