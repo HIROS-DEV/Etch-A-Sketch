c98193317a4a4b59e15ce705a4cc3ccfc3ec4d01
Zan Hiroo <95255730+HIROS-DEV@users.noreply.github.com>
2022-10-26 20:07:07 +0900

    add pseudocode(project overview) as comments in script.js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
 // => create another div using JavaScript(using createElement and add class name "grid")
 // => attach the grid div inside the container div
 // => using Css and create Grid in HTML file(I think flexbox or CSS Grid is suitable for job)
+// => Perhaps, grid width and height should be decided in advance
+
+//TODO: 2. create hover effect
+// => I think mouse enter or mouse leave or mouse over event is useful (But, it needs google)
+// => if mouse hover in grid, grid have to change color. So, background of the grid change
+
+// TODO: 3. create Grid button
+// => On page top, create button. When button click, popup shows and asking for the number of squares per side for the new grid.
+// => Existing grid should reset (or removed) and create new grid
+// => Grid number should be decided in advance (ex. max 100 x 100)
+
+// TODO: Extra
+// => Instead of just changing the color of a square from black to white (for example), have each pass through with the mouse change it to a completely random RGB value. 
